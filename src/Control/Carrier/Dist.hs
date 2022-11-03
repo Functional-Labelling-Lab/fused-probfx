f6502ededafd7853db182fe90f9dfa06ffdef6be
Charlie <charlie.lidbury@icloud.com>
2022-11-03 13:33:09 +0000

    CHARLIE PROBLEM

--- src/Control/Carrier/Dist.hs
+++ src/Control/Carrier/Dist.hs
@@ -12,14 +12,40 @@
 
 import Control.Algebra
 import Control.Effect.Dist (Dist(..))
-import Control.Effect.Sample (Sample)
+import Control.Effect.Sample (Sample(..))
 import Control.Effect.Observe (Observe)
 import Data.Kind (Type)
+import qualified Data.Map as Map
+import Data.Maybe (fromMaybe)
 
 newtype DistC a m b = DistC { runDistC :: m b }
     deriving (Functor, Applicative, Monad)
 
-instance (Has Sample sig m, Has Observe sig m, Algebra sig m) => Algebra (Dist a :+: sig) (DistC a m) where
-    alg hdl sig ctx = DistC $ case sig of
-        L (Dist d maybeY maybeTag) -> undefined -- Have to figure out this whole folding
-        R other -> alg (runDistC . hdl) other ctx
+instance forall sig m a . (Algebra sig m, Has (Sample a) sig m, Has (Observe a) sig m) => Algebra (Dist a :+: sig) (DistC a m) where
+  alg :: forall ctx n . (Algebra sig m, Functor ctx)
+      => Handler ctx n (DistC a m)
+      -> (Dist a :+: sig) n a
+      -> ctx ()
+      -> DistC a m (ctx a)
+  alg = loop 0 Map.empty
+    where
+      loop counter tagMap hdl sig@(L (Dist primDist mObs mTag)) ctx = DistC $ case mObs of
+        Just obs -> undefined
+        Nothing -> do
+          x <- send (Sample primDist (tag, tagIdx))
+          ctx' <- runDistC $ loop (counter + 1) tagMap' hdl sig ctx
+          pure $ x <$ ctx'
+        where
+          tag     = fromMaybe (show counter) mTag
+          tagIdx  = Map.findWithDefault 0 tag tagMap
+          tagMap' = Map.insert tag (tagIdx + 1) tagMap
+
+      loop counter tagMap hdl (R other) ctx = DistC $ alg hdl' other ctx
+        where
+          hdl' :: Handler ctx n m
+          hdl' x = runDistC (hdl x)
+
+              -- do 
+              -- x <- send (Observe primDist obs (tag, tagIdx))
+              -- ctx' <- runDist $ loop (counter + 1) tagMap' (hdl) sig ctx
+              -- pure $ x <$ ctx
