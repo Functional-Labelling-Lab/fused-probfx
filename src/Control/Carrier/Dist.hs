59d6894c3dedf3d0d37a7d7b29e4bc1437c339e3
Charlie <charlie.lidbury@icloud.com>
2022-11-03 15:43:35 +0000

    Zhixuan is a ledgend (fix Dist)

--- src/Control/Carrier/Dist.hs
+++ src/Control/Carrier/Dist.hs
@@ -18,35 +18,43 @@ import Control.Effect.Observe (Observe)
 import Data.Kind (Type)
 import qualified Data.Map as Map
 import Data.Maybe (fromMaybe)
+import Effects.Dist (Tag)
+import Control.Effect.Observe (Observe(..))
 
 newtype DistC a m b = DistC { runDistC :: m b }
     deriving (Functor, Applicative, Monad)
 
-instance forall sig m a . (Algebra sig m, Has (Sample a) sig m, Has (Observe a) sig m) => Algebra (Dist a :+: sig) (DistC a m) where
-  alg :: forall ctx n . (Algebra sig m, Functor ctx)
-      => Handler ctx n (DistC a m)
-      -> (Dist a :+: sig) n a
+instance (Algebra sig m, Has (Sample b) sig m, Has (Observe b) sig m) => Algebra (Dist b :+: sig) (DistC b m) where
+  alg :: forall ctx n a . Functor ctx
+      => Handler ctx n (DistC b m) --
+      -> (Dist b :+: sig) n a
       -> ctx ()
-      -> DistC a m (ctx a)
+      -> DistC b m (ctx a)
   alg = loop 0 Map.empty
     where
+      loop
+          :: Int
+          -> Map.Map Tag Int
+          -> Handler ctx n (DistC b m) --
+          -> (Dist b :+: sig) n a
+          -> ctx ()
+          -> DistC b m (ctx a)
       loop counter tagMap hdl sig@(L (Dist primDist mObs mTag)) ctx = DistC $ case mObs of
-        Just obs -> undefined
+        Just obs -> do 
+          x <- send (Observe primDist obs (tag, tagIdx))
+          ctx' <- recurCtx
+          pure $ x <$ ctx'
         Nothing -> do
           x <- send (Sample primDist (tag, tagIdx))
-          ctx' <- runDistC $ loop (counter + 1) tagMap' hdl sig ctx
+          ctx' <- recurCtx
           pure $ x <$ ctx'
         where
           tag     = fromMaybe (show counter) mTag
           tagIdx  = Map.findWithDefault 0 tag tagMap
           tagMap' = Map.insert tag (tagIdx + 1) tagMap
+          recurCtx = runDistC $ loop (counter + 1) tagMap' (hdl) sig ctx
 
       loop counter tagMap hdl (R other) ctx = DistC $ alg hdl' other ctx
         where
           hdl' :: Handler ctx n m
           hdl' x = runDistC (hdl x)
-
-              -- do 
-              -- x <- send (Observe primDist obs (tag, tagIdx))
-              -- ctx' <- runDist $ loop (counter + 1) tagMap' (hdl) sig ctx
-              -- pure $ x <$ ctx
