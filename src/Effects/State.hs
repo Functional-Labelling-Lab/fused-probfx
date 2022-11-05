7160d3da85861afd58656ddce7f47ac07747ba50
Charlie <charlie.lidbury@icloud.com>
2022-11-05 19:31:13 +0000

    refacotor: neaten up probfx state handler

--- src/Effects/State.hs
+++ src/Effects/State.hs
@@ -36,14 +36,9 @@ modify :: Member (State s) es => (s -> s) -> Prog es ()
 modify f = get >>= put . f
 
 -- | Handle the @State s@ effect
-handleState
-  :: s -- ^ initial state
-  -> Prog (State s ': es) a
-  -> Prog es (a, s) -- ^ (output, final state)
-handleState s m = loop s m where
-  loop :: s -> Prog (State s ': es) a -> Prog es (a, s)
-  loop s (Val x) = return (x, s)
-  loop s (Op u k) = case discharge u of
-    Right Get      -> loop s (k s)
-    Right (Put s') -> loop s' (k ())
-    Left  u'       -> Op u' (loop s . k)
+handleState :: s -> Prog (State s ': es) a -> Prog es (a, s)
+handleState s (Val x) = return (x, s)
+handleState s (Op u k) = case discharge u of
+  Right Get      -> handleState s (k s)
+  Right (Put s') -> handleState s' (k ())
+  Left  u'         -> Op u' (handleState s . k)
