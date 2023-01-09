8cecc1a8bedce01a8800f2c3c0929e3ab32a2737
Oliver Killane <oliverkillane@gmail.com>
2023-01-09 12:19:02 +0000

    Using new style command for ghcup ghc version change in monad-bayes benchmark

--- benchmarking/monad-bayes/bench.sh
+++ benchmarking/monad-bayes/bench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 PREVVERSION=$(ghc --version | sed -rn 's/[^[:digit:]]*([[:digit:]]+.[[:digit:]]+.[[:digit:]]+)/\1/p')
-ghcup set 8.4.4 
+ghcup set ghc 8.4.4 
 cabal run
-ghcup set ${PREVVERSION}
+ghcup set ghc ${PREVVERSION}
